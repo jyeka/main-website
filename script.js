43ed29ce7d9144b5e7267d17242be320f409ae6b
blinchik06 <michaldolata6@gmail.com>
2023-10-20 21:12:54 +0200

    working menu finally!

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+let elms = document.getElementsByClassName("navigation");
+
+function show() {
+    let width = window.innerWidth;
+    Array.from(elms).forEach((x) => {
+      if(width < 1000) {
+        x.style.width = "100%"
+      } else
+      {
+        x.style.width = "250px"
+      }
+
+})
+}
+function hide() {
+  Array.from(elms).forEach((x) => {
+            x.style.width = "0%"
+})
+}
+
