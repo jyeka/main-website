5db74a50e0b1f81e952c0f2fe28038212e782ee7
RGBprivate <mmm.game321@gmail.com>
2023-10-23 21:34:02 +0200

    Fixed broken sponsors slider.

--- script.js
+++ script.js
@@ -18,28 +18,6 @@ function hide() {
             x.style.width = "0%"
 })
 }
-const carousel = document.querySelector(".carousel");
-const arrowBtns = document.querySelectorAll(".wrapper i");
-const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-const carouselChildrens = [...carousel.children];
-
-let isDragging = false, startX, startScrollLeft;
-
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-})
-
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-})
-
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth
-    })
-})
 
 /* Przeciaganie */
 // const dragStart = (e) => {
@@ -61,19 +39,6 @@ arrowBtns.forEach(btn => {
 // carousel.addEventListener("mousedown", dragStart);
 // carousel.addEventListener("mousemove", dragging);
 // document.addEventListener("mouseup", dragStop);
-
-const infiniteScroll = () => {
-    if(carousel.scrollLeft === 40) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
-    }
-}
-carousel.addEventListener("scroll", infiniteScroll);
 
 (function () {
     function scrollHorizontally(f) {
